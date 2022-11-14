b997261c9b941e72bebf3e5fb74d7f1fd7aa3b33
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-14 18:31:50 -0500

    week 1 day 1, reverse.js completed

---
+++ week1/day1/reverse.js
@@ -0,0 +1,16 @@
+// week 1, day 1 stretch
+// reverse string
+
+const args = process.argv;
+
+args.map(((arg, index) => {
+  if (index > 1 && index < args.length) {
+
+    let output = "";
+
+    for (let i = arg.length - 1; i >= 0; i--) {
+      output += arg[i];
+    }
+    console.log(output);
+  }
+}));
