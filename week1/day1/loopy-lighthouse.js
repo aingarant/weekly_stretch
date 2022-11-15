17aac0d2d9f7fcfc162d87a6046f4229a545a535
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-14 20:34:03 -0500

    refactored loopy lighthouse

---
+++ week1/day1/loopy-lighthouse.js
@@ -0,0 +1,22 @@
+const loopyLighthouse = function(range, multiples, words) {
+  //range = start and end values of loop
+  for (let i = range[0]; i <= range[1]; i++) {
+  //multiples = which numbers you want to replace with words
+  //words = which words to replace the multiples with
+    const multiple1 = i % multiples[0] === 0;
+    const multiple2 = i % multiples[1] === 0;
+    let string = "";
+    if (multiple1 && multiple2) {
+      string = words[0] + words[1];
+    } else if (multiple2) {
+      string = words[1];
+    } else if (multiple1) {
+      string = words[0];
+    } else {
+      string = i;
+    }
+    console.log(string);
+  }
+};
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
--- week1/day1/pig-latin.js
+++ week1/day1/pig-latin.js
@@ -1,12 +1,10 @@
 const args = process.argv;
 
+
+const piglatin = (args) =>
+{
+
 args.map(((arg, index) => {
-  if (index > 1 && index < args.length) {
-
-    let output = "";
-
-    output = arg.substring(1) + arg.charAt(0) + "ay";
-
-    console.log(output);
-  }
+  if (index > 1 && index < args.length)  arg.substring(1) + arg.charAt(0) + "ay";
 }));
+}
